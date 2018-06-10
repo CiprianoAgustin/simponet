fb98a9f03756868a63a6de8b072ea7764e693e47
CiprianoAgustin <agustincipriano@hotmail.com>
2018-06-10 15:03:59 -0300

    Primera iteración de red

---
+++ net/firstnet.py
@@ -0,0 +1,64 @@
+# import the necessary packages
+from keras import backend as K
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dense
+from keras.layers.core import Dropout
+from keras.layers.core import Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
+
+
+class SmallerVGGNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # INPUT SHAPE & CHANNEL
+        model = Sequential()
+        input_shape = (height, width, depth)
+        chan_dim = -1
+        if K.image_data_format() == "channels_first":
+            input_shape = (depth, height, width)
+            chan_dim = 1
+
+        # CONV => RELU => POOL
+        model.add(Conv2D(32, (3, 3), padding="same",
+                         input_shape=input_shape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(MaxPooling2D(pool_size=(16, 16)))
+        model.add(Dropout(0.25))
+
+        # (CONV => RELU) * 2 => POOL
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(MaxPooling2D(pool_size=(8, 8)))
+        model.add(Dropout(0.25))
+
+        # (CONV => RELU) * 2 => POOL
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(MaxPooling2D(pool_size=(4, 4)))
+        model.add(Dropout(0.25))
+
+        # FC => RELU
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # softmax
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # return the constructed network architecture
+        return model
